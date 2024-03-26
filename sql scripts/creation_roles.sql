4027df8e7b9957157e9b558f7038889248f8d148
kevinoustoff <35457309+kevinoustoff@users.noreply.github.com>
2024-03-26 15:27:21 +0100

    creation des roles

---
+++ sql scripts/creation_roles.sql
@@ -0,0 +1,6 @@
+INSERT INTO roles (nom_du_role) VALUES ('ROLE_USER');
+
+INSERT INTO roles (nom_du_role) VALUES ('ROLE_ADMINISTRATEUR');
+
+INSERT INTO roles (nom_du_role) VALUES ('ROLE_NUTRITIONISTE');
+
