26c31eff9a0496ffa49f30d78d6e139ea51a60c6
kevinoustoff <35457309+kevinoustoff@users.noreply.github.com>
2024-03-27 08:22:26 +0100

    insertion manuelle des utilisateurs dans la base de données

--- sql scripts/creation_roles.sql
+++ sql scripts/creation_roles.sql
@@ -4,3 +4,12 @@ INSERT INTO roles (nom_du_role) VALUES ('ROLE_ADMINISTRATEUR');
 
 INSERT INTO roles (nom_du_role) VALUES ('ROLE_NUTRITIONISTE');
 
+
+INSERT INTO utilisateurs (nom, prenom, email, mot_de_passe, role_id, nutritionniste_id, age, token) 
+VALUES 
+-- 123456789
+('Toffa', 'kevin', 'kevin.toffa@manger.com', '25f9e794323b453885f5181f1b624d0b', 1, NULL, 30, NULL),
+-- 987654321
+('Abdoulaye', 'mamadou', 'mamadou.abdoulaye@example.com', '6ebe76c9fb411be97b3b0d48b791a7c9', 2, NULL, 25, NULL),
+-- 000000000
+('Hasinianina', 'andriamahefatsihoarana', 'parfait.hefatsi@example.com', '4c93008615c2d041e33ebac605d14b5b', 2, NULL, 35, NULL);
